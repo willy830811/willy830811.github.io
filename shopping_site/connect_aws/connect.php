8a414f9ac3328f8b23cc4273a9a54dd24ad0246a
willy830811 <willy830811@gmail.com>
2019-05-29 13:12:15 +0800

    hide the pw

--- shopping_site/connect_aws/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-mysqli_connect($_SERVER['aapoe1drvo89qb.cikflqxb7lgf.ap-southeast-1.rds.amazonaws.com'], $_SERVER['root'], $_SERVER['0911002402'], $_SERVER['aapoe1drvo89qb'], $_SERVER['3306']);//連結伺服器
-*/
-mysql_connect("http://3.1.110.238","root","0911002402");
-mysql_select_db('shopping_site');//選擇資料庫
-mysql_query("set names utf8");//讓資料可以讀取中文
-?>
